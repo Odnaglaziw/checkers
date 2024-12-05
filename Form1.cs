10b1911c9edff45cf49b15816b349753992b3f01
Odnaglaziw <odnaglazik@gmail.com>
2024-12-06 02:51:32 +0500

    тики-таки

--- Form1.cs
+++ Form1.cs
@@ -2,6 +2,7 @@ using System.Net.WebSockets;
 using System.Text;
 using System.Text.Json;
 using System.Text.Encodings.Web;
+using System;
 
 namespace checkers
 {
@@ -17,48 +18,18 @@ namespace checkers
             InitializeWebSocket(); // Инициализация WebSocket
             Upd();
             gameBoard1.MoveMade += GameBoard1_MoveMade;
+            System.Windows.Forms.Timer gameBoard1_Timer = new System.Windows.Forms.Timer();
+            gameBoard1_Timer.Interval = 16;
+            gameBoard1_Timer.Tick += GameBoard1_Timer_Tick;
+            gameBoard1_Timer.Start();
         }
 
-        private async void InitializeWebSocket()
+        private async void GameBoard1_Timer_Tick(object? sender, EventArgs e)
         {
-            client = new ClientWebSocket();
-            Uri serverUri = new Uri("ws://192.168.1.199:65000/");
-            await client.ConnectAsync(serverUri, CancellationToken.None);
-            Console.WriteLine("Соединение с сервером установлено!");
-        }
-
-        private async void GameBoard1_MoveMade(object? sender, MoveEventArgs e)
-        {
-            if (client.State != WebSocketState.Open)
-            {
-                MessageBox.Show("Соединение с сервером разорвано. Перезапустите клиент.");
-                return;
-            }
-
-            string comand = e.MoverIsBlack ? "black" : "white";
-            var options = new JsonSerializerOptions
-            {
-                Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
-            };
-
-            var message = JsonSerializer.Serialize(new
-            {
-                Action = comand,
-                From = $"{e.From.X}:{e.From.Y}",
-                To = $"{e.To.X}:{e.To.Y}",
-                IsCapture = e.IsCapture,
-                capture = $"{e.Capture.X}:{e.Capture.Y}"
-            }, options);
-
-            byte[] buffer = Encoding.UTF8.GetBytes(message);
-            await client.SendAsync(new ArraySegment<byte>(buffer), WebSocketMessageType.Text, true, CancellationToken.None);
-            Console.WriteLine($"Сообщение отправлено: {message}");
-
             // Получение ответа от сервера
-            buffer = new byte[1024];
+            byte[] buffer = new byte[1024];
             var result = await client.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
             string response = Encoding.UTF8.GetString(buffer, 0, result.Count);
-
             try
             {
                 // Десериализация JSON-ответа в объект класса Action
@@ -78,7 +49,7 @@ namespace checkers
                     {
                         capture = new Point(int.Parse(action.capture.Split(':')[0]), int.Parse(action.capture.Split(':')[1]));
                     }
-                    gameBoard1.MoveTo(from.X,from.Y,to.X,to.Y,isBlack,capture.X,capture.Y,action.iscapture);
+                    gameBoard1.MoveTo(from.X, from.Y, to.X, to.Y, isBlack, capture.X, capture.Y, action.iscapture);
                     byte[] buffer2 = Encoding.UTF8.GetBytes("Получил указания, передвинул");
                     await client.SendAsync(new ArraySegment<byte>(buffer2), WebSocketMessageType.Text, true, CancellationToken.None);
                 }
@@ -91,6 +62,39 @@ namespace checkers
             {
                 MessageBox.Show($"Ошибка десериализации JSON: {ex.Message}");
             }
+        }
+
+        private async void InitializeWebSocket()
+        {
+            client = new ClientWebSocket();
+            Uri serverUri = new Uri("ws://192.168.1.199:65000/");
+            await client.ConnectAsync(serverUri, CancellationToken.None);
+            Console.WriteLine("Соединение с сервером установлено!");
+        }
+
+        private async void GameBoard1_MoveMade(object? sender, MoveEventArgs e)
+        {
+            if (client.State != WebSocketState.Open)
+            {
+                MessageBox.Show("Соединение с сервером разорвано. Перезапустите клиент.");
+                return;
+            }
+            string comand = e.MoverIsBlack ? "black" : "white";
+            var options = new JsonSerializerOptions
+            {
+                Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+            };
+            var message = JsonSerializer.Serialize(new
+            {
+                Action = comand,
+                From = $"{e.From.X}:{e.From.Y}",
+                To = $"{e.To.X}:{e.To.Y}",
+                IsCapture = e.IsCapture,
+                capture = $"{e.Capture.X}:{e.Capture.Y}"
+            }, options);
+            byte[] buffer = Encoding.UTF8.GetBytes(message);
+            await client.SendAsync(new ArraySegment<byte>(buffer), WebSocketMessageType.Text, true, CancellationToken.None);
+            Console.WriteLine($"Сообщение отправлено: {message}");
         }
 
         private void Upd()
